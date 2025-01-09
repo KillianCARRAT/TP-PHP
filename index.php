559369d791b276e89a4d04a09d970fac856393a5
KillianCARRAT <killian.carrat@etu.univ-orleans.fr>
2025-01-09 09:57:02 +0100

    Debut acceuil

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="/public/assets/css/style.css">
+    <title>Acceuil</title>
+</head>
+
+<body>
+    <main>
+        <h1>
+            Quiz
+        </h1>
+        <p>
+
+        </p>
+    </main>
+</body>
+
+</html>
